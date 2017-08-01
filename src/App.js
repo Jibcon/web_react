57035fa5eebc59f5214e4a3ebf47c9bbf41bd5fa
pjo901018 <pjo901018@korea.ac.kr>
2017-08-01 14:55:08 +0900

    Intro Pages Update

--- src/App.js
+++ src/App.js
@@ -13,54 +13,55 @@ class IntroComponent extends Component {
   render() {
     return (
       <div className="container">
-        <header>
-          <ul>
-            <li>
-              <a href="#jibcon">
-                <span>
-                  jibcon
-                  </span>
-              </a>
-            </li>
-            <li>
-              <a href="#Recruit">
-                <span>
-                  Recruit
-                </span>
-              </a>
-            </li>
-            <li>
-              <a href="#People">
-                <span>
-                  People
-                </span>
-              </a>
-            </li>
-            <li>
-              <a href="#Contact">
-                <span>
-                  Contact
-                </span>
-              </a>
-            </li>
-          </ul>
-        </header>
-        <div>
-          <section>
-            <div id="jibcon">
-              jibcon
-            </div>
-            <div id="Recruit">
-              채용
-            </div>
-            <div id="People">
-              사람들
-            </div>
-            <div id="Contact">
-              연락
-            </div>
-          </section>
-        </div>
+        <img className="intro-image" src="https://github.com/Jibcon/web_react/blob/rebase/src/assets/170801-Landing-Page2.png?raw=true" />
+        {/*<header>*/}
+          {/*<ul>*/}
+            {/*<li>*/}
+              {/*<a href="#jibcon">*/}
+                {/*<span>*/}
+                  {/*jibcon*/}
+                  {/*</span>*/}
+              {/*</a>*/}
+            {/*</li>*/}
+            {/*<li>*/}
+              {/*<a href="#Recruit">*/}
+                {/*<span>*/}
+                  {/*Recruit*/}
+                {/*</span>*/}
+              {/*</a>*/}
+            {/*</li>*/}
+            {/*<li>*/}
+              {/*<a href="#People">*/}
+                {/*<span>*/}
+                  {/*People*/}
+                {/*</span>*/}
+              {/*</a>*/}
+            {/*</li>*/}
+            {/*<li>*/}
+              {/*<a href="#Contact">*/}
+                {/*<span>*/}
+                  {/*Contact*/}
+                {/*</span>*/}
+              {/*</a>*/}
+            {/*</li>*/}
+          {/*</ul>*/}
+        {/*</header>*/}
+        {/*<div>*/}
+          {/*<section>*/}
+            {/*<div id="jibcon">*/}
+              {/*jibcon*/}
+            {/*</div>*/}
+            {/*<div id="Recruit">*/}
+              {/*채용*/}
+            {/*</div>*/}
+            {/*<div id="People">*/}
+              {/*사람들*/}
+            {/*</div>*/}
+            {/*<div id="Contact">*/}
+              {/*연락*/}
+            {/*</div>*/}
+          {/*</section>*/}
+        {/*</div>*/}
       </div>
     );
   }
